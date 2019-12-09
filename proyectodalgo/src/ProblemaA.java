f75e7fab1215740f362b3d850a2be2b7d0d44ba4
jdcamachod <jd.camachod@uniandes.edu.co>
2019-12-09 16:43:56 -0500

    problema_A

--- proyectodalgo/src/ProblemaA.java
+++ proyectodalgo/src/ProblemaA.java
@@ -1,7 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-//Autora: Vilma Tirado Gómez
+/**
+ * Clase que presenta la solución al problema del arreglo recurrente más largo
+ * @author Juan Diego Camacho
+ * @author Daniel Perilla
+ *
+ */
 public class ProblemaA {
 
 	public static void main(String[] args) throws Exception {
@@ -17,15 +23,37 @@ public class ProblemaA {
 				line = br.readLine();
 				final String [] dataStr = line.split(" ");
 				final int[] numeros = Arrays.stream(dataStr).mapToInt(f->Integer.parseInt(f)).toArray();
-				int respuesta = instancia.buscarSubArreglo(numeros);
+				int respuesta = instancia.arregloRecurrenteMasLargo(numeros);
 				System.out.println(respuesta);
 				line = br.readLine();
 			}
 		}
 	}
 
-
-
+	/**
+	 * Algoritmo de solución que dado un arreglo busca el arreglo recurrente más largo dentro de este
+	 * @param a arreglo de números
+	 * @return El tamaño del arreglo recurrente más largo dentro de a
+	 */
+	public int arregloRecurrenteMasLargo(int[] a) {
+		int max = 0;
+		int c = -1;
+		int in = 0;
+		for(int i = 1; i< a.length; i++) {
+			for(int k = i; k<a.length; k++) {
+				if(a[k-i]!=a[k]) {
+					c = -1;
+					in = k;
+				}
+				c++;
+				if(c>max) {
+					max = c;
+				}
+				
+			}
+		}
+		return max;
+	}
 	public int buscarSubArreglo(int[] arreglo){
 		//Longitud del arreglo mas largo 
 		int l= 0;
